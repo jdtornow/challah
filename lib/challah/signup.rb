2a4f37069cbfe2722af02b9ba66fa61c3e348b87
John Tornow <john@johntornow.com>
2012-12-19 17:19:12 -0600

    Starting on tests for Signups

--- lib/challah/signup.rb
+++ lib/challah/signup.rb
@@ -4,11 +4,85 @@ module Challah
     include ActiveModel::Conversion
 
     attr_reader :errors
+    attr_accessor :provider, :user
 
     def initialize(attributes = {})
       self.user = ::User.new
       self.attributes = attributes
-      @errors = []
+      self.provider = :password
+      @errors = ActiveModel::Errors.new(self)
+    end
+
+    def attributes=(value)
+      return unless Hash === value
+
+      value.each do |key, value|
+        self.send("#{key}=", value)
+      end
+    end
+
+    def method_missing(method, *attrs, &block)
+      if user.respond_to?(method)
+        return user.send(method, *attrs, &block)
+      else
+        super
+      end
+    end
+
+    def name
+      "Signup"
+    end
+
+    def password=(value)
+      @provider = :password
+    end
+
+    def provider?
+      Challah.providers.keys.include?(@provider)
+    end
+
+    def provider
+      @provider ||= :password
+    end
+
+    def provider=(value)
+      @provider = value.to_sym
+    end
+
+    def read_attribute_for_validation(attr)
+      send(attr)
+    end
+
+    def save
+      if valid?
+        user.save
+      end
+    end
+
+    def valid?
+      if user.valid? and provider?
+        true
+      else
+        @errors = ActiveModel::Errors.new(self)
+
+        user.errors.messages.each do |k, v|
+          v.each do |msg|
+            @errors.add(k, msg)
+          end
+        end
+
+        false
+      end
+    end
+
+    class << self
+      def human_attribute_name(attr, options = {})
+        attr.to_s.humanize
+      end
+
+      def lookup_ancestors
+        [ self ]
+      end
     end
   end
 end
--- test/signup_test.rb
+++ test/signup_test.rb
@@ -1,5 +1,70 @@
 require 'helper'
 
 class SignupTest < ActiveSupport::TestCase
+  context "A user" do
+    should "sign up for an app with a password" do
+      signup = Challah::Signup.new
+      signup.first_name             = 'Avon'
+      signup.last_name              = 'Barksdale'
+      signup.email                  = 'avon@challah.me'
+      signup.password               = 'test123'
+      signup.password_confirmation  = 'test123'
 
+      assert_difference [ 'User.count', 'Authorization.count' ], 1 do
+        assert signup.save, 'Signup could not save!'
+      end
+
+      assert_equal false, signup.new_record?
+      assert_equal "Avon Barksdale", signup.user.name
+      assert_equal true, signup.user.authenticate_with_password('test123')
+      assert_equal :password, signup.provider
+    end
+
+    should "sign up using a params hash" do
+      user_params = {
+        first_name: 'Stringer',
+        last_name:  'Bell',
+        email:      'stringer@challah.me',
+        password:   'abc123',
+        password_confirmation: 'abc123'
+      }
+
+      signup = Challah::Signup.new(user_params)
+
+      assert_difference [ 'User.count', 'Authorization.count' ], 1 do
+        assert signup.save, 'Signup could not save'
+      end
+
+      assert_equal false, signup.new_record?
+      assert_equal 'Stringer Bell', signup.user.name
+      assert_equal true, signup.user.authenticate_with_password('abc123')
+    end
+
+    should "not sign up without a password" do
+      signup = Challah::Signup.new
+      signup.first_name             = 'Avon'
+      signup.last_name              = 'Barksdale'
+      signup.email                  = 'avon@challah.me'
+
+      assert_no_difference [ 'User.count', 'Authorization.count' ] do
+        assert_equal false, signup.save
+      end
+
+      assert_equal true, signup.new_record?
+      assert_equal "Avon Barksdale", signup.user.name
+      assert_equal :password, signup.provider
+    end
+
+    should "sign up with another valid provider" do
+
+    end
+
+    should "not sign up without any providers" do
+
+    end
+
+    should "not sign up with an invalid provider" do
+
+    end
+  end
 end

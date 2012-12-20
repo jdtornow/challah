d12adf8d0101960b1e7171b65dfccdfd5e1d13a0
John Tornow <john@johntornow.com>
2012-12-20 11:40:22 -0600

    Provider attribute does not need to be accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :password_confirmation, :password, :provider, :username
+  attr_accessible :email, :first_name, :last_name, :password_confirmation, :password, :username
 
   # Set up Challah's User methods. Keep this as the first line of your model to include
   # all methods by default. You can override methods after this line as necessary.

617f69552dec3230cb78a89a7626e038ab734861
John Tornow <john@johntornow.com>
2013-01-08 16:33:45 -0600

    Use explicit verb routes for Rails 4 compatibility

--- lib/challah/routes.rb
+++ lib/challah/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   unless Challah.options[:skip_routes]
-    match '/sign-in',   to: 'sessions#create',   as: 'authenticate',  via: :post
-    match '/sign-in',   to: 'sessions#new',      as: 'signin',        via: :get
-    match '/sign-out',  to: 'sessions#destroy',  as: 'signout'
+    post '/sign-in',    to: 'sessions#create',    as: 'authenticate'
+    get '/sign-in',     to: 'sessions#new',       as: 'signin'
+    get '/sign-out',    to: 'sessions#destroy',   as: 'signout'
 
-    match '/login',     to: 'sessions#create',   via: :post
-    match '/login',     to: 'sessions#new',      as: 'login',         via: :get
-    match '/logout',    to: 'sessions#destroy',  as: 'logout'
+    post '/login',      to: 'sessions#create'
+    get '/login',       to: 'sessions#new',       as: 'login'
+    get '/logout',      to: 'sessions#destroy',   as: 'logout'
   end
 
   # These are used for testing purposes only.
   if Rails.env.test?
-    match '/challah/:action', controller: 'challah/test/restrictions'
+    get '/challah/:action', controller: 'challah/test/restrictions'
   end
 end

3b7fad1d51acd810498a2b578340c8a535553584
John D. Tornow <john@johntornow.com>
2013-04-08 21:46:41 -0500

    gem updates, bug fix for named routes in rails 4

--- lib/challah/routes.rb
+++ lib/challah/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get '/sign-in',     to: 'sessions#new',       as: 'signin'
     get '/sign-out',    to: 'sessions#destroy',   as: 'signout'
 
-    post '/login',      to: 'sessions#create'
+    post '/login',      to: 'sessions#create',    as: 'submit_login'
     get '/login',       to: 'sessions#new',       as: 'login'
     get '/logout',      to: 'sessions#destroy',   as: 'logout'
   end

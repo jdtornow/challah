ead4ba9bc2eeb365aead146c9724f2b6b3c95243
John D. Tornow <john@johntornow.com>
2015-09-01 09:10:22 -0500

    Don't rely on having an ApplicationController in the root app
    Makes things work nicer when using only engines

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,42 +1,44 @@
-class SessionsController < ApplicationController
-  before_filter :destroy_session, except: :create
+if defined?(ApplicationController)
+  class SessionsController < ApplicationController
+    before_filter :destroy_session, except: :create
 
-  unloadable
+    unloadable
 
-  # GET /login
-  # GET /sign-in
-  def new
-    @session = Challah::Session.new(request)
-  end
-
-  # POST /login
-  # POST /sign-in
-  def create
-    @session = Challah::Session.new(request, params[:session])
-    @session.ip = request.remote_ip
-
-    if @session.save
-      redirect_to return_to_path
-    else
-      redirect_to signin_path, alert: I18n.translate('sessions.create.failed_login')
+    # GET /login
+    # GET /sign-in
+    def new
+      @session = Challah::Session.new(request)
     end
-  end
 
-  # GET /logout
-  # GET /sign-out
-  def destroy
-    redirect_to signin_path
-  end
+    # POST /login
+    # POST /sign-in
+    def create
+      @session = Challah::Session.new(request, params[:session])
+      @session.ip = request.remote_ip
 
-  protected
+      if @session.save
+        redirect_to return_to_path
+      else
+        redirect_to signin_path, alert: I18n.translate('sessions.create.failed_login')
+      end
+    end
 
-  def destroy_session
-    current_user_session.destroy
-  end
+    # GET /logout
+    # GET /sign-out
+    def destroy
+      redirect_to signin_path
+    end
 
-  def return_to_path(default_path = '/')
-    result = session[:return_to]
-    result = nil if result and result == "http://#{request.domain}/"
-    result || default_path
+    protected
+
+    def destroy_session
+      current_user_session.destroy
+    end
+
+    def return_to_path(default_path = '/')
+      result = session[:return_to]
+      result = nil if result and result == "http://#{request.domain}/"
+      result || default_path
+    end
   end
 end

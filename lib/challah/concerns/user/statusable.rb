4c7769af56da8cd8b6764f8de77422a89a03d694
John D. Tornow <john@johntornow.com>
2020-01-06 09:18:33 -0600

    always use symbols for statuses

--- lib/challah/concerns/user/statusable.rb
+++ lib/challah/concerns/user/statusable.rb
@@ -6,7 +6,7 @@ module Challah
       begin
         if columns.map(&:name).include?("status")
           additional_statuses = Array(Challah.options[:additional_statuses])
-          enum status: [:active, :inactive, *additional_statuses]
+          enum status: [:active, :inactive, *additional_statuses].map(&:to_sym)
         end
       rescue ActiveRecord::StatementInvalid => exception
         raise exception unless exception.message =~ /could not find table/i ||
